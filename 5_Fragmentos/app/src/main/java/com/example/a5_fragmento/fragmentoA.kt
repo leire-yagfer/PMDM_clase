48fa36e1071ba6f9cea79b6d4ae6ff48cf23fd2f
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2025-01-14 09:28:34 +0100

    primer fragmento

---
+++ 5_Fragmentos/app/src/main/java/com/example/a5_fragmento/fragmentoA.kt
@@ -0,0 +1,64 @@
+package com.example.a5_fragmento
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// private const val ARG_PARAM1 = "param1"
+// private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [fragmentoA.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class fragmentoA : Fragment() { //clase que hereda de fragment
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }*/
+    }//onCreate
+
+
+
+    override fun onCreateView( //infla el Layout
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val v = inflater.inflate(R.layout.fragment_fragmento_a, container, false)
+        return v //devuelve una vista
+    }//view
+
+
+
+    /*companion object { //zona estática de la clase
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment fragmentoA.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            fragmentoA().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }//companion object*/
+}
