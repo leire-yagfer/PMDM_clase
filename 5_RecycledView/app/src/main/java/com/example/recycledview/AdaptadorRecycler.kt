360be647603dd93659026110a90a0120bc8149a6
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2025-01-23 09:35:40 +0100

    funciona todo correctamente, salvo porque no hay SDK

---
+++ 5_RecycledView/app/src/main/java/com/example/recycledview/AdaptadorRecycler.kt
@@ -0,0 +1,53 @@
+package com.example.recycledview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+
+//VER EL PDF DEL SEGUNDO PARCIAL DE ESTE APARTADO DE RECYCLER
+class AdaptadorRecycler(private val datos: List<String>): RecyclerView.Adapter<AdaptadorRecycler.MiViewHolder>() { //clase que hereda de RecycleView
+
+    //vincula la vista con el holder y genera y retorna un objeto de tipo ViewHolder --> SE VA A EJECUTAR TANTAS VECES COMO OBJETOS HAYA EN EL RECYCLEVIEW
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MiViewHolder {
+
+        //inflar la vista del elemento --> devuelve una vista, por lo que lo almaceno en una variable
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.elements_recycle, parent, false) //false pq da error sino
+
+        //construir un objeto ViewHolder de mi propia clase --> les relaciono
+        val objeto_viewholder = MiViewHolder(vista)
+
+        //vincular --> devuelvo un objeto viewHolder
+        return objeto_viewholder
+    }//onCreateViewHolder
+
+
+    //saber cuantas veces tiene que invocar a la lista de datos
+    override fun getItemCount(): Int {
+        //retornar el numero de elemtos que tiene la lista de datos
+        return datos.size
+
+    }//getItemCount
+
+
+    //viuncular los datos con el Holder --> se reciclan cuando haga scroll --> se ejecuta tantas veces como elemntos aparezcan en la pantalla cuando haga sroll
+    override fun onBindViewHolder(holder: MiViewHolder, position: Int) {
+        //vinculo cada uno de los elementos visuales (vistas) del holder con los datos
+            //itemView == vista --> representa a la vista individual y tiene la misma referencia que vista
+        holder.render(datos.get(position)) //trato como si fuese una lista
+    }//onBindViewHolder
+
+
+    //clase que representa un ViewHolder que hereda de RecyclerView
+    class MiViewHolder(val vista: View): RecyclerView.ViewHolder(vista) {
+
+        //asociar a los elementos visuales de cada elemnto del recyclerView los datos
+        fun render(dato: String){
+            itemView.findViewById<TextView>(R.id.textViewElement).text = dato //equivalente: vista.findViewById<TextView>(R.id.textViewElement)
+        }//render
+
+    }//MiViewHolder
+}//class
--- 5_RecycledView/app/src/main/java/com/example/recycledview/MainActivity.kt
+++ 5_RecycledView/app/src/main/java/com/example/recycledview/MainActivity.kt
@@ -7,17 +7,23 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.RecyclerListener
 import com.example.recycledview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mibinding: ActivityMainBinding
+    //defino el objeto del RecyclerView
     lateinit var mirecycler: RecyclerView
+    //defino el objeto del Adapter
+    lateinit var miadapter: AdaptadorRecycler
+    //creo la lñista de datos
+    var lista_datos = mutableListOf("Mercedes", "BMW", "Renault", "Opel", "Ford")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        //binding
         mibinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mibinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -34,7 +40,13 @@ class MainActivity : AppCompatActivity() {
         //instanciar el recyclerView
         this.mirecycler = mibinding.viewMirecycler
 
-        //Definir el manejador del LAyout del recycler
-        this.mirecycler.layoutManager = LinearLayoutManager(this)
+        //Definir el manejador del Layout del recycler
+        this.mirecycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false) //LinearLayoutManager.VERTICAL --> indica como se disponen
+
+        //asignar una referencia del adapter
+        this.miadapter = AdaptadorRecycler(this.lista_datos)
+
+        //vinculo el RecyclerView al Adaptador
+        this.mirecycler.adapter = this.miadapter
     }//inicializar_recyclerview
 }//class
